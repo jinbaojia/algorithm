4f7235f3c390a7953864a74e9f296b18fb64d955
BJJIN <17856419249@163.com>
2022-04-09 19:14:50 +0800

    leetcode 二分查找模板2

--- search/binSearch.js
+++ search/binSearch.js
@@ -1,4 +1,6 @@
 //二分查找,只适合有序数组 O(lgn+1)
+
+//初级版本，查找某个一定存在的元素
 const binSearch = (array, data) => {
   let upperBound = array.length - 1;
   let lowerBound = 0;
@@ -29,5 +31,33 @@ const recursiveBinSearch = (array, data, lo, hi) => {
     return mid;
   }
 };
+
+/**
+ * 高级版本，查找每个元素的插入位置
+ * @param {*} array
+ * @param {*} data
+ * @returns
+ */
+const HiBinSearch = (array, data) => {
+  if (array === null || array.length === 0) {
+    return -1;
+  }
+
+  let left = 0;
+  let right = array.length;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (array[mid] === data) {
+      return mid;
+    } else if (array[mid] < data) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  if (left != array.length && array[left] == target) return left;//当left+1 === right时，不会进入循环，所以要判断
+  return -1;
+};
+
 const list = [1, 2, 3, 4, 5];
-console.log(recursiveBinSearch(list, 4));
+console.log(HiBinSearch(list, 2));
