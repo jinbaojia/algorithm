54006ff5fe768c12c80719fe246a047642fe7be9
金保佳 <17856419249@163.com>
2022-04-11 17:35:27 +0800

    细分左右边界的情况

--- search/binSearch.js
+++ search/binSearch.js
@@ -1,6 +1,6 @@
 //二分查找,只适合有序数组 O(lgn+1)
 
-//初级版本，查找某个一定存在的元素
+//初级版本，查找某个一定存在的元素,但是对于[1,2,2,2,3]这样的数组，如果搜索2，并不能得到，左右边界的2的索引
 const binSearch = (array, data) => {
   let upperBound = array.length - 1;
   let lowerBound = 0;
@@ -33,10 +33,7 @@ const recursiveBinSearch = (array, data, lo, hi) => {
 };
 
 /**
- * 高级版本，查找每个元素的插入位置
- * @param {*} array
- * @param {*} data
- * @returns
+ * 寻找左侧边界的二分搜索
  */
 const binSearch2 = (array, data) => {
   if (array === null || array.length === 0) {
@@ -48,15 +45,38 @@ const binSearch2 = (array, data) => {
   while (left < right) {
     const mid = Math.floor(left + (right - left) / 2);
     if (array[mid] === data) {
-      return mid;
+      right = mid; //重点
     } else if (array[mid] < data) {
       left = mid + 1;
     } else {
       right = mid;
     }
   }
-  if (left != array.length && array[left] == target) return left; //当left+1 === right时，不会进入循环，所以要判断
+  if (left != array.length && array[left] == data) return left; //当left+1 === right时，不会进入循环，所以要判断
   return -1;
+};
+
+/**
+ * 寻找右侧边界的二分搜索
+ */
+const binSearch3 = (array, data) => {
+  if (array === null || array.length === 0) {
+    return -1;
+  }
+
+  let left = 0;
+  let right = array.length;
+  while (left < right) {
+    const mid = Math.floor(left + (right - left) / 2);
+    if (array[mid] === data) {
+      left = mid + 1; //重点
+    } else if (array[mid] < data) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return array[left-1] === data ? (left-1) : -1;
 };
 
 /**
@@ -68,7 +88,7 @@ const binSearch2 = (array, data) => {
  * @param {*} array
  * @param {*} data
  */
-const binSearch3 = (array, data) => {
+const binSearch4 = (array, data) => {
   if (array === null || array.length === 0) {
     return -1;
   }
@@ -88,5 +108,5 @@ const binSearch3 = (array, data) => {
   if (nums[right] == data) return right;
 };
 
-const list = [1, 2, 3, 4, 5];
-console.log(binSearch2(list, 2));
+const list = [1, 2, 2, 2, 3];
+console.log(binSearch4(list, 2));
