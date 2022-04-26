b6fa27d87129215bc5c7188f6b803927ad6a38d5
金保佳 <17856419249@163.com>
2022-04-26 18:03:53 +0800

    合并两个有序数组解法2

--- search/binSearch/test.js
+++ search/binSearch/test.js
@@ -20,53 +20,37 @@ const findMedianSortedArrays_Array_Sort = function (nums1, nums2) {
 //维护两个指针，初始时分别指向两个数组的下标 00 的位置，每次将指向较小值的指针后移一位
 //（如果一个指针已经到达数组末尾，则只需要移动另一个数组的指针），直到到达中位数的位置。
 
-const findMedianSortedArrays_Pointer = function (nums1, nums2) {
-  const nums1Len = nums1.length - 1;
-  const nums2Len = nums2.length - 1;
-  let p1 = 0;
-  let p2 = 0;
-  const midNum = (nums1Len + nums2Len + 1) / 2;
-  const mid = Math.floor(midNum);
-  let i = 1;
-  while (i <= mid) {
-    if (p1 >= nums1Len) {
-      p2++;
-    } else if (p2 >= nums2Len) {
-      p1++;
-    } else {
-      const n1 = nums1[p1];
-      const n2 = nums2[p2];
-      if (n1 > n2) {
-        p2++;
+var findMedianSortedArrays = function(nums1, nums2) {
+  let n1 = nums1.length;
+  let n2 = nums2.length;
+
+  // 两个数组总长度
+  let len = n1 + n2;
+
+  // 保存当前移动的指针的值(在nums1或nums2移动)，和上一个值
+  let preValue = -1;
+  let curValue = -1;
+
+  //  两个指针分别在nums1和nums2上移动
+  let point1 = 0;
+  let point2 = 0;
+
+  // 需要遍历len/2次，当len是奇数时，最后取curValue的值，是偶数时，最后取(preValue + curValue)/2的值
+  for (let i = 0; i <= Math.floor(len/2); i++) {
+      preValue = curValue;
+      // 需要在nums1上移动point1指针
+      if (point1 < n1 && (point2 >= n2 || nums1[point1] < nums2[point2])) {
+          curValue = nums1[point1];
+          point1++;
       } else {
-        p1++;
+          curValue = nums2[point2];
+          point2++;
       }
-    }
-    i++;
   }
-
-  if (mid === midNum) {
-    if (p1 >= nums1Len) {
-      return nums2[p2];
-    }
-    if (p2 >= nums2Len) {
-      return nums1[p1];
-    }
-
-    if (nums1[p1] > nums2[p2]) {
-      return nums1[p1];
-    } else {
-      return nums2[p2];
-    }
-  } else {
-    if (p1 >= nums1Len) {
-      return (nums2[p2] + nums2[p2 + 1]) / 2;
-    }
-    if (p2 > nums2Len) {
-      return (nums2[p1] + nums2[p1 + 1]) / 2;
-    }
-    return (nums1[p1] + nums2[p2]) / 2;
-  }
+  
+  return len % 2 === 0 
+      ? (preValue + curValue) / 2
+      : curValue
 };
 
-console.log(findMedianSortedArrays_Pointer([1, 5], [2, 6]));
+console.log(findMedianSortedArrays([1, 5], [2, 6]));
