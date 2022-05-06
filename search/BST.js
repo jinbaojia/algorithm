1dbf93321bb8e819db0833727aa050c8937854bb
金保佳 <17856419249@163.com>
2022-05-06 16:17:31 +0800

    BST

--- search/BST.js
+++ search/BST.js
@@ -1,27 +1,46 @@
 class Node {
-  key;
-  val;
-  left;
-  right;
-  n;
-  constructor(key, val, n) {
-    this.key = key;
-    this.val = val;
-    this.n = n;
+  left = null;
+  right = null;
+  value;
+  constructor(value, left, right) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
   }
 }
+
 class BST {
-  root;
-
-  constructor() {}
-  size(node) {
-    if (!node) {
-      return 0;
+  root = null;
+  insert(data) {
+    const node = new Node(data);
+    if (this.root === null) {
+      this.root = node;
+      return;
     }
-    return node.n;
+    let curNode = this.root;
+    let parent;
+    while (curNode) {
+      parent = curNode;
+      if (curNode.value > data) {
+        curNode = curNode.left;
+      } else {
+        curNode = curNode.right;
+      }
+    }
+    if (parent.value > data) {
+      parent.left = node;
+    } else {
+      parent.right = node;
+    }
   }
-  get(key) {
-  
 
-  }
+  inOrderRecursively() {}
 }
+
+const arr = [65, 23, 54, 1, 3, 659, 23, 5, 8, 334, 68, 09, 674, 26, 568, 234];
+const bst = new BST();
+for (const iterator of arr) {
+  bst.insert(iterator);
+}
+
+console.log(bst);
