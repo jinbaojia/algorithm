375930c9a16e28b9802970faf54b4dc92318ee34
BJJIN <17856419249@163.com>
2022-05-09 21:55:35 +0800

    中序遍历

--- search/BST.js
+++ search/BST.js
@@ -2,15 +2,20 @@ class Node {
   left = null;
   right = null;
   value;
+
   constructor(value, left, right) {
     this.value = value;
-    this.left = left;
-    this.right = right;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
   }
 }
 
+/**
+ * 前中后遍历指的是 根节点的遍历顺序
+ */
 class BST {
   root = null;
+
   insert(data) {
     const node = new Node(data);
     if (this.root === null) {
@@ -34,13 +39,31 @@ class BST {
     }
   }
 
-  inOrderRecursively() {}
+  /**
+   * 中序遍历-递归
+   * @param node
+   */
+  inOrderRecursively(node) {
+    if (node === null) { return; }
+    this.inOrderRecursively(node.left);
+    console.log(node.value);
+    this.inOrderRecursively(node.right);
+  }
+
+  /**
+   * 中序遍历-迭代
+   */
+  inOrder(node) {
+    if (!node) { return; }
+    const queue = [node];
+
+  }
 }
 
-const arr = [65, 23, 54, 1, 3, 659, 23, 5, 8, 334, 68, 09, 674, 26, 568, 234];
+const arr = [23,45,16,37,3,99,22];
 const bst = new BST();
 for (const iterator of arr) {
   bst.insert(iterator);
 }
-
 console.log(bst);
+bst.inOrderRecursively(bst.root);
