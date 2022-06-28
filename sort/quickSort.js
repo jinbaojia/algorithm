70d2f053e07df13bedc5e370fbb77e905b9f0a31
BJJIN <17856419249@163.com>
2022-06-28 08:43:58 +0800

    快排尾递归优化

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,6 +1,7 @@
-const quickSort = (array, start, end) => {
-  start = typeof start !== "number" ? 0 : start;
-  end = typeof end !== "number" ? array.length - 1 : end;
+//尾递归优化
+const quickSortImpl = (array, stack) => {
+  const start = stack[0];
+  const end = stack[1];
   if (start >= end) {
     return;
   }
@@ -29,13 +30,67 @@ const quickSort = (array, start, end) => {
   if (i === j && array[i] < pivot) {
     i++;
   }
-
-  quickSort(array, start, i - 1);
-  quickSort(array, i, end);
+  stack.shift();
+  stack.shift();
+  if (start < i - 1) {
+    stack.unshift(start, i - 1);
+  }
+  if (i < end) {
+    stack.unshift(i, end);
+  }
+  if (stack.length === 0) {
+    return;
+  }
+  return quickSortImpl(array, stack);
 };
+
+var sortArray = function (nums) {
+  const quickSort = (array, start, end) => {
+    start = typeof start !== "number" ? 0 : start;
+    end = typeof end !== "number" ? array.length - 1 : end;
+    if (start >= end) {
+      return;
+    }
+
+    let i = start;
+    let j = end;
+    let pivot = array[Math.floor((start + end) / 2)];
+    while (i < j) {
+      //找右边第一个小于等于pivot
+      while (array[j] > pivot) {
+        j--;
+      }
+
+      //找左边第一个大于等于pivot
+      while (array[i] < pivot) {
+        i++;
+      }
+      if (i <= j) {
+        [array[i], array[j]] = [array[j], array[i]];
+        i++;
+        j--;
+      }
+    }
+
+    if (i === j && array[i] < pivot) {
+      i++;
+    }
+
+    quickSort(array, start, i - 1);
+    quickSort(array, i, end);
+  };
+  quickSort(nums);
+  return nums;
+};
+
+const quickSort = (array) => {
+  let stack = [0, array.length - 1];
+  quickSortImpl(array, stack);
+};
+
 const items = [-1, 2, -8, -10];
 const result = quickSort(items);
-console.log(result);
+console.log(items);
 
 /**
  * 快排最好的情况每次正好中分O(nlogn)
