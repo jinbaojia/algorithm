55e693d5b742b8bd20ef2d0e45d034eaf81d438f
BJJIN <17856419249@163.com>
2022-06-28 08:33:18 +0800

    重新理解快排

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,46 +1,37 @@
-const partition = (array, left, right) => {
-  /**
-   * 有许多方法可以计算基准值。
-   * 一些算法选择第一个数作为基准值。这不是最好的选择，
-   * 因为它在已经排序的数组上给出了最差的性能(O(n^2)的时间复杂度，而平均时间复杂度是O(nlogn))。
-   * 最好在数组中间选择一个基准值
-   */
-  const pivot = array[Math.floor((right + left) / 2)];
-  //这里的等于是因为，不能确定left === right 时的值 是大于还是小于基准值，所以走后门的逻辑让left++;
-  //让left和right相等时的值放在左边，也就是按小于基准值的情况
-  while (left <= right) {
-    while (array[left] < pivot) {
-      //不是array[left] <= pivot 是为了防止数组中的值全都大于等于或者小于等于pivot
-      left++;
-    }
-    while (array[right] > pivot) {
-      right--;
-    }
-    if (left <= right) {
-      //这里的等于是为了 数组是有序的，如果没有等于，有序的数组将会死循环
-      [array[left], array[right]] = [array[right], array[left]];
-      left++;
-      right--;
-    }
-  }
-  return left;
-};
-const quickSort = (array, left, right) => {
-  if (array.length === 0) {
-    return array;
-  }
-  typeof left !== "number" && (left = 0);
-  typeof right !== "number" && (right = array.length - 1);
-
-  const index = partition(array, left, right);
-  if (left < index - 1) {
-    quickSort(array, left, index - 1);
-  }
-  if (index < right) {
-    quickSort(array, index, right);
+const quickSort = (array, start, end) => {
+  start = typeof start !== "number" ? 0 : start;
+  end = typeof end !== "number" ? array.length - 1 : end;
+  if (start >= end) {
+    return;
   }
 
-  return array;
+  let i = start;
+  let j = end;
+  let pivot = array[Math.floor((start + end) / 2)];
+  while (i < j) {
+    //找右边第一个小于等于pivot
+    while (array[j] > pivot) {
+      j--;
+    }
+
+    //找左边第一个大于等于pivot
+    while (array[i] < pivot) {
+      i++;
+    }
+    if (i <= j) {
+      //等于是数组有序的情况下避免死循环
+      [array[i], array[j]] = [array[j], array[i]];
+      i++;
+      j--;
+    }
+  }
+  //i与j相等时，如果array[i]<pivot就i++;保证左边都小于pivot，右边都大于pivot
+  if (i === j && array[i] < pivot) {
+    i++;
+  }
+
+  quickSort(array, start, i - 1);
+  quickSort(array, i, end);
 };
 const items = [-1, 2, -8, -10];
 const result = quickSort(items);
