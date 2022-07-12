d6e0186d1ff8aa9b1610719dfc9c23dd5d26d2d5
BJJIN <17856419249@163.com>
2022-07-12 22:23:28 +0800

    递归版后序遍历

--- tree/binaryTree/postOrder.js
+++ tree/binaryTree/postOrder.js
@@ -4,14 +4,26 @@ const postOrder = (treeNode) => {
     return;
   }
   postOrder(treeNode.left);
-  console.log(treeNode.val);
   postOrder(treeNode.right);
+  console.log(treeNode.val);
 };
-// inOrder(treeNode);
+// postOrder(treeNode);
 
 const postOrderIterator = (treeNode) => {
   if (!treeNode) {
     return;
   }
   const stack = [treeNode];
+  const outputStack = [];
+  while (stack.length) {
+    const node = stack.pop();
+    outputStack.push(node);
+    node.right && stack.push(node.left);
+    node.left && stack.push(node.right);
+  }
+  while (outputStack.length) {
+    const node = outputStack.pop();
+    console.log(node.val);
+  }
 };
+postOrderIterator(treeNode);
